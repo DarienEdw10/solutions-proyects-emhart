9d55aeb8c366b1ae238b0afa81babf5712cd3013
DarienEdw10 <darienedwin.jimenez@partner.magna.com>
2026-08-10 08:34:54 -0600

    Actualizacion de Program.cs en La API cambio de generico a no generico

--- Enhartt.Api/Program.cs
+++ Enhartt.Api/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Enhartt.Domain.Data; // Importación corregida
+using Enhartt.Domain.Data;
 using Enhartt.Domain.Repositories;
 using Enhartt.Api.Services;
 
@@ -9,10 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("CadenaSql")));
 
-// 2. Registrar Inyección de Dependencias para Repositorios y Servicios
-builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+// 2. Registrar Inyección de Dependencias para Repositorios y Servicios (NO GENÉRICO)
+builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddScoped<IParametroService, ParametroService>();
 builder.Services.AddScoped<IRecetaService, RecetaService>();
+
 // 3. Configurar CORS para permitir peticiones desde la interfaz web
 builder.Services.AddCors(options =>
 {
