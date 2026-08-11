3991267dfcff0505eabbc2b0c655258618061740
DarienEdw10 <darienedwin.jimenez@partner.magna.com>
2026-08-11 12:15:54 -0600

    Actualizacion Creacion de Salidas con Respecto a La Celda

--- Enhartt.MVC/obj/Debug/net8.0/Enhartt.MVC.AssemblyInfo.cs
+++ Enhartt.MVC/obj/Debug/net8.0/Enhartt.MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enhartt.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8de2b3dcef238fd07a93dc9ded3c6ee36ed862e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88ecd757eef8e0a3e5792ab69a296f112c1e127b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enhartt.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enhartt.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
